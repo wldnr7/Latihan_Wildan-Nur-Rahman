06abd32b15ceacf5de2e6ac2d5d4cdd23b5964f4
Wildan Nur Rahman <80866806+wldnr7@users.noreply.github.com>
2021-03-21 19:08:24 +0700

    Add files via upload

---
+++ Matematika.java
@@ -0,0 +1,31 @@
+package com.matematik;
+
+public class Matematika {
+
+
+    void pertambahan (int a, int b){
+
+        int hasil = a + b;
+
+        System.out.printf("Pertambahan : %d + %d = %d\n",a,b,hasil);
+    }
+    void pengurangan (int a, int b){
+
+        int hasil = a - b;
+
+        System.out.printf("Pengurangan : %d - %d = %d\n",a,b,hasil);
+    }
+    void perkalian (int a, int b){
+
+        int hasil = a * b;
+
+        System.out.printf("Perkalian : %d * %d = %d\n",a,b,hasil);
+    }
+    void pembagian (int a, int b){
+
+        int hasil = a / b;
+
+        System.out.printf("Pembagian : %d / %d = %d\n",a,b,hasil);
+    }
+
+}
---
+++ matematikaBeraksi.java
@@ -0,0 +1,31 @@
+package com.matematik;
+
+public class matematikaBeraksi {
+
+    public static void main(String[] args) {
+
+        //membuat object
+        Matematika math = new Matematika();
+
+            //memanggil method
+
+        //pertambahan
+        math.pertambahan(20,20);
+
+        //Pengurangan
+        math.pengurangan(10,5);
+
+        //perkalian
+        math.perkalian(10,20);
+
+        //pembagian
+        math.pembagian(20,2);
+
+
+
+
+
+
+
+    }
+}
