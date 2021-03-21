fab6e3cf78de03622a2b5d9a5f4d12d2e933b113
Wildan Nur Rahman <80866806+wldnr7@users.noreply.github.com>
2021-03-21 18:54:10 +0700

    Add files via upload

---
+++ halo/HaloIndonesia.java
@@ -0,0 +1,11 @@
+package com.halo;
+
+public class HaloIndonesia {
+
+    public static void main(String[] args) {
+
+        System.out.println("Halo Indonesia");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");
+    }
+}
