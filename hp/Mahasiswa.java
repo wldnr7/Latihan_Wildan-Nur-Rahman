1b8ef783c563df7335d99f557d2ec3bb37f62a24
Wildan Nur Rahman <80866806+wldnr7@users.noreply.github.com>
2021-03-21 18:58:17 +0700

    Add files via upload

---
+++ hp/Mahasiswa.java
@@ -0,0 +1,14 @@
+package com.siswa;
+
+public class Mahasiswa {
+
+    void membaca(){
+        System.out.println("Mahasiswa sedang membaca..");
+    }
+    void menyontek(){
+        System.out.println("Mahasiswa mencontek dalam ujian..");
+    }
+    void modifikasi(){
+        System.out.println("Mahasiswa memodifikasi source code..");
+    }
+}
