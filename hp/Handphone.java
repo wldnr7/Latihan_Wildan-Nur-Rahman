500607cbaff9627efa1e9e2b7b67fdad0bf4ebce
Wildan Nur Rahman <80866806+wldnr7@users.noreply.github.com>
2021-03-21 18:56:55 +0700

    Add files via upload

---
+++ hp/Handphone.java
@@ -0,0 +1,21 @@
+package com.hp;
+
+public class Handphone {
+
+    void hidupkan(){
+        System.out.println("Handphone hidup...");
+    }
+
+    void lakukanPanggilan(){
+        System.out.println("Kring, kring, kring... panggilan dilakukan");
+    }
+
+    void kirimSMS(){
+        System.out.println("Dung, dung... SMS berhasil dikirim");
+    }
+
+    void matikan(){
+        System.out.println("Handphone dimatikan..");
+    }
+
+}
---
+++ hp/HandphoneBeraksi.java
@@ -0,0 +1,22 @@
+package com.hp;
+
+public class HandphoneBeraksi {
+
+    public static void main(String[] args) {
+
+        //membuat object
+        Handphone handphone1 = new Handphone();
+
+            //memanggil method
+
+        //menghidupkan handphone
+        handphone1.hidupkan();
+        //melakukan panggilan
+        handphone1.lakukanPanggilan();
+        //mengirim SMS
+        handphone1.kirimSMS();
+        //menghidupkan handphone
+        handphone1.matikan();
+
+    }
+}
